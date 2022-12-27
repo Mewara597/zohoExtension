5186c9f3b16f4c40086260ad0891d3f1de1a3f51
SIDDARTH <83221360+Mewara597@users.noreply.github.com>
2022-12-28 00:02:54 +0530

    Add files via upload

---
+++ inject-script.js
@@ -0,0 +1,15 @@
+if (location.hostname == "people.zoho.com") {
+    console.log("tension mat lo me sambhal lunga")
+    const url = "https://people.zoho.com/httpsspicegemscom/zp#home/dashboard"
+
+    setInterval(() => {
+        const hours = new Date().getHours();
+        const minutes = new Date().getMinutes();
+        if (((hours == 23) && (minutes == 47)) || ((hours == 19) && (minutes == 30))) {
+            if (window.location.href == url) {
+                const signIn = document.querySelector("#ZPD_Top_Att_Stat");
+                signIn ? signIn.click() : '';
+            } else window.open(url)
+        }
+    }, 40000)
+}
